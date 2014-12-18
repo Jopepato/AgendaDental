4ab6fa31ff9cb4544855e9ed18a37db94bbee077
José Pérez-Parras Toledano <i32petoj@uco.es>
2014-12-18 14:51:35 +0100

    Funcion de mostrar un cliente terminada, falta mejorarla un poco

---
+++ Implementacion/AgendaJose.cpp
@@ -0,0 +1,88 @@
+/************************************************************
+En este fichero vamos a implementar las funciones de imprimir
+*************************************************************/
+#include <list>
+#include "Cliente.h"
+//Tengo que aprender POO para poder hacer esto
+
+
+
+using namespace std;
+
+
+
+//Esta función imprimirá los 10 clientes mas buscado por la secretaria
+std::list<Cliente> mostrarClientesMasBuscados(){
+  //Esta funcion dependerá de la funcion de imprimre cliente
+
+
+
+
+
+
+
+
+}
+
+//Esta función mostrará los datos del cliente por pantalla
+
+//Imprime cliente se beneficia de
+
+std::list<Cliente> imprimirClientes(){
+
+  //Pf no se hacer listas esto va a estar complicado :c
+
+  //Ruben me hace bullying y no se que hace D:
+
+
+
+}
+
+//Esta función recibe un cliente de parámetro y lo imprime de forma bonita
+void imprimeCliente(Clinte C){
+
+//Declaramos unas estructuras auxiliares para facilitarnos las cosas
+Direcciones dir;
+Telefonos tlf;
+Correos  cor;
+RedesSociales redes;
+
+  //Mostramos el nombre
+    cout<< C.getNombre <<", "<< C.getApellidos() << "\n";
+
+  //Mostramos el dni
+    cout<< "-" << C.getDNI()<< "\n";
+
+  //Mostramos las direcciones
+    dir = C.getDirecciones();
+    cout << "Direccion:\n";
+    cout << dir.calle << ", " << dir.puerta << ", " << dir.portal
+         << "\n";
+
+  //Mostramos los telefonos
+
+    tlf = C.getTelefonos();
+    cout << "Telefonos:\n";
+    cout << "- Teléfono fijo:\t" <<tlf.numeroFijo << "\n";
+    cout << "- Teléfono móvil:\t" << tlf.numeroMovil1 << "\n";
+    cout << "- Teléfono auxiliar:\t" << tlf.numeroMovil2 << "\n";
+
+  //Mostramos los correos
+
+    cor = C.getCorreos();
+    cout << "Correos electrónicos:\n";
+    cout << "- Gmail:\t" << cor.gmail << "\n";
+    cout << "- Hotmail:\t" << cor.hotmail << "\n";
+    cout << "- Yahoo:\t" << cor.yahoo << "\n";
+
+  //Mostramos las redes sociales
+
+    redes = C.getRedes();
+    cout << "Redes sociales:\n";
+    cout << "- Twitter:\t" << redes.twitter << "\n";
+    cout << "- Facebook:\t" << redes.facebook << "\n";
+    cout << "- Tuenti:\t" << redes.tuenti << "\n";
+    cout << "- Linkedin:\t" << redes.linkedin << "\n";
+
+
+}
