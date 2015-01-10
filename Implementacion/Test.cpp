0e097074916ec3a727b5e47f72d817d842e6f371
RubenMZ <i32mezar@uco.es>
2015-01-10 19:06:20 +0100

    Creando tests

---
+++ Implementacion/Test.cpp
@@ -0,0 +1,33 @@
+// jugador_unittest.cc: Juan A. Romero
+// A sample program demonstrating using Google C++ testing framework.
+//
+// This sample shows how to write a more complex unit test for a class
+// that has multiple member functions.
+//
+// Usually, it's a good idea to have one test for each method in your
+// class.  You don't have to do that exactly, but it helps to keep
+// your tests organized.  You may also throw in additional tests as
+// needed.
+
+#include "Agenda.h"
+#include "Cliente.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(Agenda, insertar) {
+
+}
+
+TEST(Agenda, borrado) {
+
+}
+
+TEST(Agenda, modificado) {
+
+}
+
+TEST(Agenda, existenClientes) {
+
+}
+
